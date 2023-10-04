511549040cbca8c372e8f9f9c4427d1e4d745f5c
matiasfnunez <mattarg2@gmail.com>
2023-10-04 00:05:14 -0300

    update

--- apps/web/app/api/message/route.ts
+++ apps/web/app/api/message/route.ts
@@ -4,14 +4,18 @@ import { firestore } from '../../_core/firebase/firebase-admin';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- description
 export async function POST(req: Request): Promise<any> {
 	const db = firestore();
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment -- description
-	const body: any = await req.json();
 
-	// eslint-disable-next-line no-console -- description
+  	// eslint-disable-next-line no-console -- description
 	console.log('request', JSON.stringify(req));
 
 	// eslint-disable-next-line no-console -- description
 	console.log('formData', await req.formData());
+
+  	// eslint-disable-next-line no-console -- description
+	console.log('body', req.body);
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment -- description
+	const body: any = await req.json();
 
 	await createOne(body, db, 'messages');
 
