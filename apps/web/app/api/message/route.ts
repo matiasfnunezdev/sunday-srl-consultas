06d51922fc24ebdf1bf42bbf37815a75d1803833
matiasfnunez <mattarg2@gmail.com>
2023-10-03 23:56:45 -0300

    update endpoint

--- apps/web/app/api/message/route.ts
+++ apps/web/app/api/message/route.ts
@@ -1,21 +1,26 @@
-import { createOne } from "../../_core/firebase/collection-helpers";
-import { firestore } from "../../_core/firebase/firebase-admin"
+import { createOne } from '../../_core/firebase/collection-helpers';
+import { firestore } from '../../_core/firebase/firebase-admin';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- description
 export async function POST(req: Request): Promise<any> {
- 
-  const db = firestore();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment -- description
-  const body: any = await req.json();
-  await createOne(body, db, 'messages');
-   
-  return new Response('Message added', {
-    status: 200,
-  })
+	const db = firestore();
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment -- description
+	const body: any = await req.json();
+
+	// eslint-disable-next-line no-console -- description
+	console.log('request', JSON.stringify(req));
+
+	// eslint-disable-next-line no-console -- description
+	console.log('formData', await req.formData());
+
+	await createOne(body, db, 'messages');
+
+	return new Response('Message added', {
+		status: 200,
+	});
 }
 
- 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- description
 export function GET(): any {
-  return "hello world"
+	return 'hello world';
 }
