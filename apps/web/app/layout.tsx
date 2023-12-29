ec6b2bb1b994b133fd7151a672916a1fd6d67eaa
matiasfnunez <mattarg2@gmail.com>
2023-12-29 00:55:13 -0300

    update

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,7 +4,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import { Providers } from './providers';
 
-export const dynamic = "force-dynamic";
+export const revalidate = 0
 
 const gothicBold = localFont({
 	src: [
