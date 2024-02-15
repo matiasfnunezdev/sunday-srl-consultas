12e9a5fba4c7b66a063446b7b6feeb269bc49431
matiasfnunez <mattarg2@gmail.com>
2024-02-15 10:51:22 -0300

    fix build

--- apps/web/app/_core/components/chat/chatComponents/ResponderModal.tsx
+++ apps/web/app/_core/components/chat/chatComponents/ResponderModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- N/A */
 /* eslint-disable func-names -- N/A */
 
 /* eslint-disable @typescript-eslint/no-confusing-void-expression -- description */
