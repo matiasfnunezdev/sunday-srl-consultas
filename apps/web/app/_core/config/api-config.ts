5cf98553642682b020e0f8d4efc9b4cfe8e1adc1
matiasfnunez <mattarg2@gmail.com>
2024-03-22 13:26:34 -0300

    update

--- apps/web/app/_core/config/api-config.ts
+++ apps/web/app/_core/config/api-config.ts
@@ -1,2 +1,2 @@
-// export const apiBaseUrl = 'https://staging.sundaysrl.com/api';
-export const apiBaseUrl = 'http://localhost:3000/api';
+export const apiBaseUrl = 'https://staging.sundaysrl.com/api';
+// export const apiBaseUrl = 'http://localhost:3000/api';
